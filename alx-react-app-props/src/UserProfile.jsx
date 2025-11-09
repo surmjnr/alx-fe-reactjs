2e5d175a1e991a2e8f5e168152ed6f1834b23907
Banafo <surmjnr@gmail.com>
2025-11-09 21:43:50 +0000

    Add UserProfile component to display user information using UserContext

--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import UserContext from '../UserContext';
+import UserContext from './UserContext';
 
 function UserProfile() {
     const userData = useContext(UserContext);
